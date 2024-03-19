25f5e0512360287f2895f49f3c81466756cfda37
usme <jstracking@outlook.com>
2024-03-19 16:18:47 -0500

    o

--- conexion.js
+++ conexion.js
@@ -1,16 +1,28 @@
-const mysql = require('mysql')
+const mysql = require('mysql');
 //put here the credentials of access
 const connection = mysql.createConnection({
 host: 'localhost',
 user: 'root',
 password: '',
 database: 'jstracking'
-})
+});
 connection.connect((err) =>{
     if(err) throw err // not connected!
     console.log('Connected to MySQL')
-})
-connection.query('SELECT * FROM usuarios', (error,result) =>{
-    if(error) throw error
-    console.log('los registros son:', result)
-})
+});
+
+function select() {
+    connection.query('SELECT * FROM usuarios', (error,result) =>{
+        if(error) throw error
+        console.log('los registros son:', result[2])
+    });
+}
+select();
+
+function instert(){
+    connection.query('INSERT INTO usuarios (nombre, correo, contrasena) VALUES (?,?,?)', 
+    ['juli', 'juli@juli', 'juli'], (error) =>{
+        if(error)  throw error
+        console.log("datos ingresados correctamente")
+    });
+}
