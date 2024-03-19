2988fb8d68f0cb78e2bc369324a751028e4417c4
usme <jstracking@outlook.com>
2024-03-19 00:24:39 -0500

    a

--- conexion.js
+++ conexion.js
@@ -1,8 +1,12 @@
-const mysql = require("mysql");
+const mysql = require('mysql')
 //put here the credentials of access
-module.exports = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "jstracking",
-    database: "jstracking"
-});
+const connection = mysql.createConnection({
+host: 'localhost',
+user: 'root',
+password: '',
+database: 'jstracking'
+})
+connection.connect((err) =>{
+    if(err) throw err // not connected!
+    console.log('Connected to MySQL')
+})
