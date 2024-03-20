b5e0323a166a0bcadb8f9dfed6719970c16d5d4f
usme <jstracking@outlook.com>
2024-03-20 13:49:13 -0500

    new change 20 de marzo

---
+++ public/login/assets/newuser.js
@@ -0,0 +1,14 @@
+
+const conexion = require("../conexion")
+module.exports = {
+    insertar(nombre, correo, contrasena){
+        return new Promise((resolve, reject) => {
+        conexion.query(`insert into ususarios (nombre,correo,contrasena)
+        values (:nombre,:correo,:contrasena)`, 
+        [nombre,correo,contrasena], (err, resultados) => {
+            if (err) reject(err);
+            else resolve(resultados.insrId);
+        });
+        });
+    }
+}
