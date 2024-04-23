0d5a3897cc915d86f8ef89b065ccbfc73dd3eb35
usme <jstracking@outlook.com>
2024-04-23 18:30:46 -0500

    INSETTS

--- app.js
+++ app.js
@@ -239,9 +239,11 @@ app.post("/informacionsalud", (req, res) => {
   const DOCUMENTO = req.body.DOCUMENTO;
   const FORMALERGIAS = req.body.ALERGIAS;
   const FORMCONDICIONESMEDICAS = req.body.CONDICIONESMEDICAS;
-  console.log('DOCUMENTO:', DOCUMENTO);
-  console.log('FORMALERGIAS:', FORMALERGIAS);
-  console.log('FORMCONDICIONESMEDICAS:', FORMCONDICIONESMEDICAS);
+  const FORMMEDICAMENTOS = req.body.MEDICAMENTOS;
+  console.log("DOCUMENTO:", DOCUMENTO);
+  console.log("FORMALERGIAS:", FORMALERGIAS);
+  console.log("FORMCONDICIONESMEDICAS:", FORMCONDICIONESMEDICAS);
+  console.log("FORMMEDICAMENTOS", FORMMEDICAMENTOS);
   //obtener idusuario
   function obtenerIdUsuario(DOCUMENT) {
     return new Promise((resolve, reject) => {
@@ -251,12 +253,13 @@ app.post("/informacionsalud", (req, res) => {
           reject(err);
         } else {
           const IDUSUARIO = resultadoqueryusuario[0].USUARIOID;
-          console.log("usuario", IDUSUARIO);
+          console.log("USUARIO: ", IDUSUARIO);
           resolve(IDUSUARIO);
         }
       });
     });
   }
+
   //insertar en informacion salud y obtener id
   function insertarInformacionSalud(IDUSUARIO) {
     return new Promise((resolve, reject) => {
@@ -268,13 +271,14 @@ app.post("/informacionsalud", (req, res) => {
             reject(err);
           } else {
             const IDINFORMACIONSALUD = resultadoinsertinformacion.insertId;
-            console.log("idinformacion", IDINFORMACIONSALUD);
+            console.log("IDINFORACIONSALUD: ", IDINFORMACIONSALUD);
             resolve(IDINFORMACIONSALUD);
           }
         }
       );
     });
   }
+
   //insertar alergias
   function insertarAlergias(ALERGIAS, IDINFORMACIONSALUD) {
     return new Promise((resolve, reject) => {
@@ -293,7 +297,7 @@ app.post("/informacionsalud", (req, res) => {
                     reject(err);
                   } else {
                     const IDALERGIA = resultadoinsertalergia.insertId;
-                    console.log("alergia", IDALERGIA);
+                    console.log("ALERGIA: ", IDALERGIA);
                     connection.query(
                       "INSERT INTO alergias_informacionsalud (ALERGIAID, INFORMACIONSALUDID)VALUES (?,?)",
                       [IDALERGIA, IDINFORMACIONSALUD]
@@ -303,18 +307,18 @@ app.post("/informacionsalud", (req, res) => {
               );
             } else {
               const IDALERGIA = resultadoqueryalergia[0].ALERGIAID;
-              console.log("alergia", IDALERGIA);
+              console.log("ALERGIA: ", IDALERGIA);
               connection.query(
                 "INSERT INTO alergias_informacionsalud (ALERGIAID, INFORMACIONSALUDID)VALUES (?,?)",
                 [IDALERGIA, IDINFORMACIONSALUD]
               );
             }
           }
-        }
-      );
+        });
       });
     });
   }
+
   //insertar las condiciones medicas
   function insertarCondicionesMedicas(CONDICIONESMEDICAS, IDINFORMACIONSALUD) {
     return new Promise((resolve, reject) => {
@@ -336,7 +340,7 @@ app.post("/informacionsalud", (req, res) => {
                     } else {
                       const IDCONDICIONMEDICA =
                         resultadoinsertcondicionmedica.insertId;
-                      console.log("condicion medica", IDCONDICIONMEDICA);
+                      console.log("CONDICION MEDICA: ", IDCONDICIONMEDICA);
                       connection.query(
                         "INSERT INTO condicionesmedicas_informacionsalud (CONDICIONMEDICAID, INFORMACIONSALUDID)VALUES (?,?)",
                         [IDCONDICIONMEDICA, IDINFORMACIONSALUD]
@@ -347,7 +351,7 @@ app.post("/informacionsalud", (req, res) => {
               } else {
                 const IDCONDICIONMEDICA =
                   resultadoquerycondicionmedica[0].CONDICIONMEDICAID;
-                console.log(IDCONDICIONMEDICA);
+                console.log("CONDICION MEDICA: ", IDCONDICIONMEDICA);
                 connection.query(
                   "INSERT INTO condicionesmedicas_informacionsalud (CONDICIONMEDICAID, INFORMACIONSALUDID)VALUES (?,?)",
                   [IDCONDICIONMEDICA, IDINFORMACIONSALUD]
@@ -360,17 +364,59 @@ app.post("/informacionsalud", (req, res) => {
     });
   }
 
-  obtenerIdUsuario(DOCUMENTO)
-  .then(async (IdUsuario) => {
-    const InformacionSaludId = await insertarInformacionSalud(IdUsuario);
+  //insertar las medicamentos
+  function insertarMedicamentos(MEDICAMENTOS, IDINFORMACIONSALUD) {
+    return new Promise((resolve, reject) => {
+      MEDICAMENTOS.forEach((MEDICAMENTO) => {
+        const medicamentoquery = `SELECT MEDICAMENTOID FROM medicamentos WHERE MEDICAMENTO = '${MEDICAMENTO}'`;
+        connection.query(medicamentoquery, (err, resultadoquerymedicamento) => {
+          if (err) {
+            reject(err);
+          } else {
+            if (resultadoquerymedicamento.length === 0) {
+              connection.query(
+                "INSERT INTO medicamentos (MEDICAMENTO) VALUES(?)",
+                [MEDICAMENTO],
+                (err, resultadoinsertmedicamento) => {
+                  if (err) {
+                    reject(err);
+                  } else {
+                    const IDMEDICAMENTO = resultadoinsertmedicamento.insertId;
+                    console.log("MEDICAMENTOS: ", IDMEDICAMENTO);
+                    connection.query(
+                      "INSERT INTO medicamentos_informacionsalud (MEDICAMENTOID, INFORMACIONSALUDID)VALUES (?,?)",
+                      [IDMEDICAMENTO, IDINFORMACIONSALUD]
+                    );
+                  }
+                }
+              );
+            } else {
+              const IDMEDICAMENTO = resultadoquerymedicamento[0].MEDICAMENTOID;
+              console.log("MEDICAMENTO: ", IDMEDICAMENTO);
+              connection.query(
+                "INSERT INTO medicamentos_informacionsalud (MEDICAMENTOID, INFORMACIONSALUDID)VALUES (?,?)",
+                [IDMEDICAMENTO, IDINFORMACIONSALUD]
+              );
+            }
+          }
+        });
+      });
+    });
+  }
 
-    const CondicionesMedicas = await insertarCondicionesMedicas(['FORMCONDICIONESMEDICAS'], InformacionSaludId);
-    
-    const Alergias = await insertarAlergias(['FORMALERGIAS'], InformacionSaludId);
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-  });
+  obtenerIdUsuario(DOCUMENTO)
+    .then(async (IdUsuario) => {
+      const InformacionSaludId = await insertarInformacionSalud(IdUsuario);
+
+      const promesas = [
+        insertarCondicionesMedicas(FORMCONDICIONESMEDICAS, InformacionSaludId),
+        insertarAlergias(FORMALERGIAS, InformacionSaludId),
+        insertarMedicamentos(FORMMEDICAMENTOS, InformacionSaludId),
+      ];
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 });
 
 //configurar el puerto parar el servidor
