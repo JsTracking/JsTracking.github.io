df3f478e0a1a13e603fe0f7d05188da64848f0ac
usme <jstracking@outlook.com>
2024-04-24 13:17:12 -0500

    a

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 //importar libreria
 const express = require("express");
 const mysql = require("mysql");
+const path = require('path');
 //put here the credentials of access
 const connection = mysql.createConnection({
   host: "localhost",
@@ -19,6 +20,7 @@ const app = express();
 
 //objetos dinamicos
 app.set("view engine", "ejs");
+
 //ruta para objetos dinamicos
 
 //registro
@@ -35,18 +37,12 @@ app.get("/login", function (req, res) {
 });
 
 //ruta archivos estaticos (indexes), es decir, paginas sin conexion a base de datos
-app.use(express.static("public"));
-app.get("/map", function (req, res) {
-  res.render("public/map/index.html");
-});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 //metodo para obtener datos de una pagina
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-//ruta archivos dinamicos
-//app.get('/', function(req, res) {
-//    res.render('registro');
-//});
 
 //enviar formulario a la ruta /validar
 app.post("/validar", function (req, res) {
@@ -80,7 +76,7 @@ app.post("/validar", function (req, res) {
         throw error;
       } else console.log("datos ingresados correctamente");
       //REDIRECCIONAR A LA PAGINA INICIO
-      res.redirect("/");
+      //res.redirect("/");
     }
   );
 });
@@ -230,7 +226,7 @@ app.post("/direccion", function (req, res) {
       console.error(err);
     });
   //REDIRECCIONAR A LA PAGINA INICIO
-  res.redirect("/");
+  //res.redirect("/");
 });
 
 //informacion salud
@@ -417,6 +413,9 @@ app.post("/informacionsalud", (req, res) => {
     .catch((error) => {
       console.error("Error:", error);
     });
+
+    //redireccionar al mapa
+    res.redirect('/map/index.html');
 });
 
 //configurar el puerto parar el servidor
